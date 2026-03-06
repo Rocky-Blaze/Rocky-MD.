6e7964d7a1484f5c7db60f4982237cde8bb83190
Rocky-Blaze <kodjieevans@gmail.com>
2026-03-06 18:44:12 -0500

    index.js
    Added a comprehensive menu with various commands for user interaction.

---
+++ index.js
@@ -0,0 +1,224 @@
+│✠ ROCKY-𝗠𝗗🌹〄
+│✠ ─────────────────
+│✠ ✓ *𝕺𝖊𝖛𝖊𝖑𝖔𝖕𝖊𝖗* : *ROCKY*
+│✠ ✓ *ᴘʀᴇғɪx* : .
+│✠ ✓ *ᴜsᴇʀ* : 
+│✠ ✓ *𝖁𝖊𝖗𝖘𝖎𝖔𝖓* : 1.0.0
+│✠ ✓ *ᴜᴘᴛɪᴍᴇ* : 21 hours, 42 minutes, 32 seconds
+│✠ ✓ *ᴘʟᴀᴛғᴏʀᴍ* : GITHUB 
+┗┅┅┅┅┅┅┅┅┅┅┅➢
+ROCKY-MD 🌹last longer than your last relantionship🤭 thanks to my creator 
+☤︎ 𝕲𝖗𝖔𝖚𝖕 🤭 menu ☤
+│➾ +hidetag
+│➾ +tagall
+│➾ +demote
+│➾ +promote
+│➾ +mute
+│➾ +unmute
+│➾ +join
+│➾ +kick
+│➾ +left
+│➾ +add
+│➾ +creategroup
+│➾ +resetlink
+│➾ +tag
+│➾ +listadmins
+│➾ +listonline
+│➾ +closetime
+│➾ +opentime
+│➾ +antilink
+│➾ +grouplink
+│➾ +kickadmins
+│➾ +kickall
+│➾ +welcome
+│➾ +hijack
+┗┅┅┅┅┅┅┅┅┅┅┅➢
+
+☤ ᴅᴏᴡɴʟᴏᴀᴅ ᴍᴇɴᴜ ☤
+│➾ +play
+│➾ +play2
+│➾ +vv
+│➾ +vv2
+│➾ +tiktok
+│➾ +toimg
+│➾ +ytsearch
+│➾ +movie
+│➾ +tomp3
+│➾ +tomp4
+│➾ +tourl
+│➾ +apk
+│➾ +pdftotext
+│➾ +qrcode
+│➾ +shorturl
+│➾ +say
+┗┅┅┅┅┅┅┅┅┅┅┅➢
+
+☤ ᴀɴɪᴍᴇ ᴍᴇɴᴜ ☤︎
+│➾ +rwaifu
+│➾ +waifu
+│➾ +animekill
+│➾ +animelick
+│➾ +animebite
+│➾ +animeglomp
+│➾ +animehappy
+│➾ +animedance
+│➾ +animecringe
+│➾ +animehighfive
+│➾ +animepoke
+│➾ +animewink
+│➾ +animesmile
+│➾ +animesmug
+│➾ +animewlp
+│➾ +animesearch
+│➾ +animeavatar
+┗┅┅┅┅┅┅┅┅┅┅┅➢
+
+☤︎ sᴛɪᴄᴋᴇʀ ᴍᴇɴᴜ ☤
+│➾ +sticker
+│➾ +cry
+│➾ +kill
+│➾ +hug
+│➾ +happy
+│➾ +dance
+│➾ +handhold
+│➾ +highfive
+│➾ +slap
+│➾ +kiss
+│➾ +blush
+│➾ +bite
+│➾ +cuddle
+│➾ +furbrat
+│➾ +shinobu
+│➾ +bonk
+│➾ +pat
+│➾ +nom
+┗┅┅┅┅┅┅┅┅┅┅┅➢
+
+☤ ᴠᴏɪᴄᴇ ᴍᴇɴᴜ ☤
+│➾ +bass
+│➾ +blown
+│➾ +earrape
+│➾ +deep
+│➾ +fast
+│➾ +nightcore
+│➾ +reverse
+│➾ +robot
+│➾ +slow
+│➾ +smooth
+│➾ +squirrel
+┗┅┅┅┅┅┅┅┅┅┅┅➢
+
+☤︎ ɢғx / ʟᴏɢᴏ ᴍᴇɴᴜ ☤︎
+│➾ +gfx
+│➾ +gfx2
+│➾ +gfx3
+│➾ +gfx4
+│➾ +gfx5
+│➾ +gfx6
+│➾ +gfx7
+│➾ +gfx8
+│➾ +gfx9
+│➾ +gfx10
+│➾ +gfx11
+│➾ +gfx12
+┗┅┅┅┅┅┅┅┅┅┅┅➢
+
+☤ ᴇᴘʜᴏᴛᴏ ᴍᴇɴᴜ ☤︎
+│➾ +glitchtext
+│➾ +writetext
+│➾ +advancedglow
+│➾ +typographytext
+│➾ +pixelglitch
+│➾ +neonglitch
+│➾ +flagtext
+│➾ +flag3dtext
+│➾ +deletingtext
+│➾ +blackpinkstyle
+│➾ +glowingtext
+│➾ +underwatertext
+│➾ +logomaker
+│➾ +cartoonstyle
+│➾ +papercutstyle
+│➾ +watercolortext
+│➾ +effectclouds
+│➾ +blackpinklogo
+│➾ +gradienttext
+│➾ +summerbeach
+│➾ +luxurygold
+│➾ +multicoloredneon
+│➾ +sandsummer
+│➾ +galaxywallpaper
+│➾ +style1917
+│➾ +makingneon
+│➾ +royaltext
+│➾ +freecreate
+│➾ +galaxystyle
+│➾ +createlogo
+│➾ +lighteffects
+┗┅┅┅┅┅┅┅┅┅┅┅➢
+
+☤︎ ꜰᴜɴ ᴍᴇɴᴜ ☤
+│➾ +8ball
+│➾ +trivia
+│➾ +joke
+│➾ +truth
+│➾ +dare
+│➾ +meme
+│➾ +advice
+│➾ +urban
+│➾ +moviequote
+│➾ +funfact
+│➾ +dog
+│➾ +cat
+│➾ +fact
+│➾ +coffee
+┗┅┅┅┅┅┅┅┅┅┅┅➢
+
+☤︎ ɢᴀᴍᴇ ᴍᴇɴᴜ ☤
+│➾ +rps
+│➾ +guess
+│➾ +coin
+│➾ +dice
+│➾ +hangman
+│➾ +tictactoe
+┗┅┅┅┅┅┅┅┅┅┅┅➢
+
+☤ ᴏᴛʜᴇʀs ᴍᴇɴᴜ ☤
+│➾ +Idch
+│➾ +react-ch
+│➾ +jid
+│➾ +dictionary
+│➾ +getpp
+│➾ +wiki
+│➾ +ai
+│➾ +openai
+│➾ +qc
+│➾ +readqr
+│➾ +genpass
+│➾ +myip
+│➾ +iplookup
+│➾ +currency
+│➾ +time
+│➾ +recipe
+│➾ +horoscope
+│➾ +book
+│➾ +remind
+│➾ +mathfact
+│➾ +sciencefact
+│➾ +calculate
+│➾ +weather
+┗┅┅┅┅┅┅┅┅┅┅┅➢
+
+☤ ᴏᴡɴᴇʀ ᴍᴇɴᴜ ☤
+│➾ +setpp
+│➾ +owner
+│➾ +repo
+│➾ +ban
+│➾ +unban
+│➾ +block
+│➾ +unblock
+│➾ +alive
+│➾ +ping
+│➾ +self
+│➾ +public
+┗┅┅┅┅┅┅┅┅┅┅┅➢
